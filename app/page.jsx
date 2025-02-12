fd757f229d2042dd0d81a874e6fde02189e10974
Nasir-Shaikh7 <contentcreatorns7@gmail.com>
2025-02-11 19:28:31 -0700

    bg colour

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Calculator from '@/components/Calculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-500 py-12 px-4">
+      <Calculator />
+    </main>
+  );
+}
